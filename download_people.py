76e4e882c68aa21f0d1bc476c39899cfbc3aa4f1
Nick Edgar <nick.edgar@shopify.com>
2019-09-28 14:44:09 -0400

    Download parliamentarians

---
+++ download_people.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import requests
+
+parl_api_url = 'https://lop.parl.ca/ParlinfoWebAPI'
+people_search_url = parl_api_url + '/Person/SearchAndRefine'
+
+current_parliament = 43
+
+regular_cols = ['PersonId', 'LastName', 'UsedFirstName', 'StraightDisplayName', 'Gender', 'LanguageEn',
+    'PartyEn', 'ConstituencyEn', 'ProvinceEn', 'TypeOfParliamentarianEn', 
+    'DateOfBirth', 'DateOfBirthIsApproximate', 
+    'CityOfBirthEn', 'ProvinceOfBirthEn', 'CountryOfBirthEn', 'IsCanadianOrigin', 'DiedInOffice']
+death_cols = ['DateOfDeath', 'DeceasedOnDuty']
+date_cols = ['DateOfBirth', 'DateOfDeath']
+
+def trim(s):
+    return s.lstrip('<br>').rstrip('<br>').replace('<br>', '|') if isinstance(s, str) else s
+
+def query_people(parliament):
+    params = { 'refiners': '4-%s,' % (current_parliament - parliament + 1) }
+    r = requests.get(people_search_url, params= params)
+    # print(r.url)
+    # print(r.text)
+    r.raise_for_status()
+    
+    for d in r.json():
+        row = {'Parliament': parliament}
+        row.update({col: trim(d[col]) for col in regular_cols})
+        dd = d['Death']
+        if dd:
+            row.update({col: trim(dd[col]) for col in death_cols})
+        yield row
+
+def read_people_df(parliament):
+    df = pd.DataFrame(query_people(parliament), columns= regular_cols + death_cols)
+    for col in date_cols:
+        df[col] = pd.to_datetime(df[col]).dt.date
+    return df.set_index('PersonId')
+
+def download_people(parliament):
+    filename = 'parliament-%d-people.csv' % parliament
+    print("Downloading people for parliament #%d to %s..." % (parliament, filename))
+    df = read_people_df(parliament)
+    df.to_csv(filename, encoding='utf8')
+
+for parliament in range(40, current_parliament + 1):
+    download_people(parliament)
