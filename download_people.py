270f507406c5ad94ccf76fee44a8009774714aba
Nick Edgar <nick.edgar@shopify.com>
2019-10-06 12:16:34 -0400

    Downloaded parliaments

--- download_people.py
+++ download_people.py
@@ -1,3 +1,4 @@
+import json
 import pandas as pd
 import requests
 
@@ -22,8 +23,10 @@ def query_people(parliament):
     # print(r.url)
     # print(r.text)
     r.raise_for_status()
-    
-    for d in r.json():
+    return r.json()
+
+def iter_people(people, parliament):    
+    for d in people:
         row = {'Parliament': parliament}
         row.update({col: trim(d[col]) for col in regular_cols})
         dd = d['Death']
@@ -31,17 +34,24 @@ def query_people(parliament):
             row.update({col: trim(dd[col]) for col in death_cols})
         yield row
 
-def read_people_df(parliament):
-    df = pd.DataFrame(query_people(parliament), columns= regular_cols + death_cols)
+def people_df(people, parliament):
+    df = pd.DataFrame(iter_people(people, parliament), columns= regular_cols + death_cols)
     for col in date_cols:
         df[col] = pd.to_datetime(df[col]).dt.date
     return df.set_index('PersonId')
 
 def download_people(parliament):
+    print(f"Downloading people for parliament #{parliament}...")
+    people = query_people(parliament)
+
+    filename = 'parliament-%d-people.json' % parliament
+    to_save = {'parliament': parliament, 'people': people}
+    with open(filename, 'w') as f:
+        json.dump(to_save, f, indent=2)
+
     filename = 'parliament-%d-people.csv' % parliament
-    print("Downloading people for parliament #%d to %s..." % (parliament, filename))
-    df = read_people_df(parliament)
+    df = people_df(people, parliament)
     df.to_csv(filename, encoding='utf8')
 
-for parliament in range(40, current_parliament + 1):
+for parliament in range(1, current_parliament + 1):
     download_people(parliament)
